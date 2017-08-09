0046a019246bf6510e4007327ae1f6d77dda6d71
J.D <onelastjedi@protonmail.com>
2017-08-09 23:16:22 +0200

    :sparkles: Added: login/logout feature

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -7,10 +7,22 @@
 
 <script>
 import AuthForm from '@/components/AuthForm'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     AuthForm,
+  },
+  computed: mapState({
+    currentUser: state => state.user.current,
+  }),
+  watch: {
+    currentUser() {
+      if (this.currentUser) this.$router.push('/')
+    },
+  },
+  beforeMount() {
+    if (this.currentUser) this.$router.push('/')
   },
 }
 </script>
