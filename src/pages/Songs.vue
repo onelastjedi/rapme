0cc63a8a8205c6685895c4f7ce2dd4b4d1e43c12
J.D <onelastjedi@protonmail.com>
2017-08-11 16:36:12 +0200

    :sparkles: Added: song author and createdAt (from now)
     #8

--- src/pages/Songs.vue
+++ src/pages/Songs.vue
@@ -6,8 +6,15 @@
     <template v-else>
       <ul class="songs-list">
         <li class="songs-list-item" v-for="s in songs">
-          <span class="score">{{s.score}}</span>
+          <span class="score">{{ s.score }}</span>
           <router-link :key="s.id" :to="{name: 'Song', params: {id: s.id}}">{{s.title}}</router-link>
+          <br>
+          <span class="meta">
+            <span class="by">
+              by <a href="#">{{ s.author.nickname }}</a>
+            </span>
+            <span class="time">{{ s.createdAt | moment("from") }}</span>
+          </span>
         </li>
       </ul>
     </template>
@@ -47,5 +54,15 @@ export default {
       width 80px
       text-align center
       margin-top -10px
+
+    .meta
+      font-size .85em
+      color #828282
+
+      a
+        color #828282
+
+        &:hover
+          color #000
 
 </style>
