ddaaeda6b8f678a5236e533d709a38f5f7afd245
J.D <onelastjedi@protonmail.com>
2017-09-01 21:17:20 +0200

    Changed: linter template.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,32 +9,18 @@ module.exports = {
   env: {
     browser: true,
   },
-  extends: 'airbnb-base',
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
   // required to lint *.vue files
   plugins: [
     'html'
   ],
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
-  },
   // add your custom rules here
   'rules': {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // disallow use of semicolons instead of ASI
-    'semi': 0,
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
